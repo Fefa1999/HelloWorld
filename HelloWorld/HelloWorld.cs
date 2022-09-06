c3dfa8a1b9d8e669904baadfd5c762ad722281b9
Fefa1999 <fefa@itu.dk>
2022-09-06 16:52:26 +0200

    Implemented error handling

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -3,20 +3,29 @@
 public class HelloWorld {
     public static void Main(string[] args) {
         Console.WriteLine("Enter year and press Enter");
-        int year = Int32.Parse(Console.ReadLine());
-        Boolean isLeapYear = IsLeapYear(year);
+        commandLineInputChecker(Console.ReadLine());
+    }
+
+    public static void commandLineInputChecker(String yearAsString){
+        try{
+            int year = Int32.Parse(yearAsString);
+            Boolean isLeapYear = IsLeapYear(year);
             if (isLeapYear) {
                 Console.WriteLine("yay");
             } else {
                 Console.WriteLine("nay");
             } 
-
+        }catch{
+            Console.WriteLine("Error - Please enter valid input (int)");
+        }
     }
     
     public static bool IsLeapYear(int year) {
-       if (year % 100 == 0) {
+        if(year<1582) return false;
+
+        if (year % 100 == 0) {
             return (year % 400 == 0);
         }
         return (year % 4) == 0;
-    }
+        }
 }
