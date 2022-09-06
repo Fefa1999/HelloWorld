4816e28af7f7942f4ed5d9c77019470bba72a8a8
Fefa1999 <fefa@itu.dk>
2022-09-06 16:29:55 +0200

    Created user interface for IsLeapYear.

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorld;
+
+public class HelloWorld {
+    public static void Main(string[] args) {
+        Console.WriteLine("Enter year and press Enter");
+        int year = Int32.Parse(Console.ReadLine());
+        Boolean isLeapYear = IsLeapYear(year);
+            if (isLeapYear) {
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("nay");
+            } 
+
+    }
+    
+    public static bool IsLeapYear(int year) {
+       if (year % 100 == 0) {
+            return (year % 400 == 0);
+        }
+        return (year % 4) == 0;
+    }
+}
